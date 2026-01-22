509e7e91f3733f84d78261afa9264fec0396cf8d
glgsysyoshimoto <glgsysyoshimoto@gmail.com>
2026-01-22 14:31:28 +0900

    Issue1

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,9 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      # 2026/01/22 ハッシュロケットからシンボル型に記載を変更
+      days = { month: (@todays_date + x).month,date: (@todays_date+x).day,plans: today_plans}
+      # days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
       @week_days.push(days)
     end
 
