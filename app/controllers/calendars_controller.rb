48bd72f5f13ac1ac182e77d2a3951e1b41f2bde2
glgsysyoshimoto <glgsysyoshimoto@gmail.com>
2026-01-28 22:05:30 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -22,6 +22,7 @@ class CalendarsController < ApplicationController
     # params.require(:calendars).permit(:date, :plan)
   end
 
+
   def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
@@ -38,9 +39,44 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+
+      # 2026/01/28 
+      # wday_num = # wdayメソッドを用いて取得した数値
+      wday_num = @todays_date.wday + x
+      
+      if wday_num >= 7
+      # if #「wday_numが7以上の場合」という条件式
+        wday_num = wday_num -7
+      end
+      
+
+      # 2026/01/28 
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num]}
+      # days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      
       @week_days.push(days)
     end
+
+  # def getWeek
+  #   wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+
+  #   # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
+  #   @todays_date = Date.today
+  #   # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
+
+  #   @week_days = []
+
+  #   plans = Plan.where(date: @todays_date..@todays_date + 6)
+
+  #   7.times do |x|
+  #     today_plans = []
+  #     plans.each do |plan|
+  #       today_plans.push(plan.plan) if plan.date == @todays_date + x
+  #     end
+  #     days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+  #     @week_days.push(days)
+  #   end
 
   end
 end
